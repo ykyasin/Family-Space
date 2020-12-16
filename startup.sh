53aac051a08cf252ff2715336102172f59e59d52
Yusuf-Yasin <ykyasin1996@gmail.com>
2020-12-16 14:39:01 +0000

    updated startup again

--- startup.sh
+++ startup.sh
@@ -9,6 +9,6 @@ pip3 install -r requirements.txt
 sudo mkdir /opt/Family-Space
 sudo chown -R jenkins /opt/Family-Space
 
-sudo system daemon-reload
+sudo systemctl daemon-reload
 sudo systemctl stop family-space.service
 sudo systemctl start family-space.service
