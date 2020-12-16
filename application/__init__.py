bd8927d9e6db87c763921a9f23716bf680ef29d4
Yusuf-Yasin <ykyasin1996@gmail.com>
2020-12-16 16:35:58 +0000

    updated init

--- application/__init__.py
+++ application/__init__.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-
+db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 #app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 #app.config['SECRET_KEY'] = "the pass"
-db = SQLAlchemy(app)
+
 
 from application import routes
