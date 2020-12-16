f7d5831fa8da976462bcec5e37b958aef23ad2ff
Yusuf-Yasin <ykyasin1996@gmail.com>
2020-12-16 15:34:46 +0000

    another update

--- startup.sh
+++ startup.sh
@@ -2,8 +2,6 @@
 sudo apt-get update
 sudo apt-get install python3
 sudo apt-get install python3-venv -y
-python3 -m venv venv
-source venv/bin/activate
 cd $WORKSPACE 
 ls -lh 
 
@@ -11,9 +9,13 @@ sudo cp -r $WORKSPACE /opt/jenkins
 sudo chown -R jenkins /opt/jenkins
 cd ~ 
 cd /opt/jenkins
+python3 -m venv venv
+source /opt/jenkins/venv/bin/activate
 
 pip3 install -r fs-app/requirements.txt 
 
 
 sudo systemctl daemon-reload
+sudo systemctl stop app.service
 sudo systemctl start app.service
+echo "works"
