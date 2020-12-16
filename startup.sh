1a7860bec0f75c176851700cedb618ed2df40a2a
Yusuf-Yasin <ykyasin1996@gmail.com>
2020-12-16 14:58:38 +0000

    updated startup again

--- startup.sh
+++ startup.sh
@@ -4,6 +4,17 @@ sudo apt-get install python3
 sudo apt-get install python3-venv -y
 cd $WORKSPACE 
 ls -lh 
+
+sudo cp -r $WORKSPACE /opt/jenkins
+sudo chown -R jenkins /opt/jenkins
+cd ~ 
+cd /opt/jenkins
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install -r requirements.txt 
+pip3 -list
 # python3 -m venv venv
 # source venv/bin/activate
 # pip3 install -r requirements.txt
