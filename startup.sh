d1d0b5a810d25da8d4da85b3707c5221074acfda
Yusuf-Yasin <ykyasin1996@gmail.com>
2020-12-16 21:00:08 +0000

    space

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 sudo apt update
 sudo apt-get install python3-venv
-sudo apt-get install python3-flask
-python3 -m venv venv
+
 cd $WORKSPACE
 ls -lah
 sudo cp -r $WORKSPACE /opt/jenkins
 sudo chown -R jenkins /opt/jenkins
+python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
