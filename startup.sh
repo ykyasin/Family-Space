cd583584ab1938a4ed26a0c0234602a4250f7dde
Yusuf-Yasin <ykyasin1996@gmail.com>
2020-12-16 21:23:47 +0000

    space

--- startup.sh
+++ startup.sh
@@ -12,15 +12,18 @@ sudo cp -r fs-app /opt/jenkins
 sudo chown -R jenkins /opt/jenkins
 cd /opt/jenkins/fs-app
 python3 -m venv venv
-ls
+source venv/bin/activate
 #cd $WORKSPACE
 #ls -lah
 #sudo cp -r $WORKSPACE /opt/jenkins
 #sudo chown -R jenkins /opt/jenkins
 #python3 -m venv venv
 #source venv/bin/activate
+echo "my breaak <------------------------------------------->"
+echo "my breaak <------------------------------------------->"
+echo "my breaak <------------------------------------------->"
 pip3 install -r requirements.txt
-
+pip3 list
 
  
 #sudo chown -R jenkins /opt/jenkins
