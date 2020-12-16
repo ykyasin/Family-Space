fbde3d34e04451cf18808467b0fc2f1069a37edb
Yusuf-Yasin <ykyasin1996@gmail.com>
2020-12-16 14:27:47 +0000

    updated startup

--- startup.sh
+++ startup.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "hello"
+sudo apt update
+sudo apt-get install python3-venv -y
+
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+sudo mkdir /opt/Family-Space
+sudo chown -R jenkins /opt/Family-Space
+
+sudo system daemon-reload
+sudo systemctl stop family-space.service
+sudo systemctl start family-space.service
