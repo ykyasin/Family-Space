024b9fd243e521b77a36c106a6cca9439fbd4c2e
Yusuf-Yasin <ykyasin1996@gmail.com>
2020-12-21 17:38:29 +0000

    fixed error

--- application/forms.py
+++ application/forms.py
@@ -4,7 +4,7 @@ from wtforms_sqlalchemy.fields import QuerySelectField
 from application.models import User
 
 class Login(FlaskForm):
-    users = QuerySelectField(query_factory=choice_user, allow_blank=True, get_label='name')
+    users = QuerySelectField(query_factory=choice_user(), allow_blank=True, get_label='name')
 
 def  choice_user():
     return User.query
