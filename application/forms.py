3d4f4548d002d6834d8e1493bfc9a4d5ce99a0d8
Yusuf-Yasin <ykyasin1996@gmail.com>
2020-12-21 17:40:20 +0000

    fixed error

--- application/forms.py
+++ application/forms.py
@@ -3,11 +3,12 @@ from wtforms import StringField, SubmitField
 from wtforms_sqlalchemy.fields import QuerySelectField
 from application.models import User
 
-class Login(FlaskForm):
-    users = QuerySelectField(query_factory=choice_user(), allow_blank=True, get_label='name')
-
 def  choice_user():
     return User.query
+
+class Login(FlaskForm):
+    users = QuerySelectField(query_factory=choice_user, allow_blank=True, get_label='name')
+
 
 
 class PostForm(FlaskForm):
