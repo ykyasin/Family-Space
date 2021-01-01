f782b5d83a461116a0acc1b40a0b0a09939a52cd
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-01 21:19:04 +0000

    adding delete post function 14

--- application/routes.py
+++ application/routes.py
@@ -22,7 +22,7 @@ def main(user = "No User"):
         db.session.commit()
         return redirect(url_for('main', user=user))
     
-    if delpost.validate_on_submit(): 
+    if delpost.post.data: 
         post = delpost.post.data
         return post
 
