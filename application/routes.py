e1dcdd2ce89700b131bd5835f223ff62372709c3
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-02 16:26:56 +0000

    login part disappears if no users 2

--- application/routes.py
+++ application/routes.py
@@ -87,7 +87,7 @@ def main(user):
 def login(): #Add users
     form = Login()
     formuser= UserForm()
-    are_users = db.query(User).first()
+    are_users = SELECT True FROM User LIMIT 1;
     if request.method == 'POST':
         if formuser.name.data:
             name = formuser.name.data
