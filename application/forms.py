185da57f6e194d42397e231dbe3d9c494acb0650
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-02 22:43:04 +0000

    validators

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, HiddenField
 from wtforms_sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.validators import DataRequired, Length, ValidationError, InputRequired
 from application.models import User
 
 def  choice_user():
