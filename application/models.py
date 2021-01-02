f3e60fbe104d0726aa0561a19a2b45dbb23ccd09
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-02 17:17:11 +0000

    Adding datetime to posts

--- application/models.py
+++ application/models.py
@@ -9,4 +9,5 @@ class User(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     detail = db.Column(db.String(180))
+    date_time = db.column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
