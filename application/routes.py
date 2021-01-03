8ec9b17e0007cddef7571015a394bd3649b14358
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-03 21:42:08 +0000

    Numbering posts id

--- application/routes.py
+++ application/routes.py
@@ -16,11 +16,12 @@ def main(user):
     name_change = False
     delete_account = False
     post_db = Post.query.order_by(Post.id).all()
+    range_posts = len(post_db)
     posts = []
     posts_id = []
     post_time = []
     users = []
-    for i in range(len(post_db)):
+    for i in range(range_posts):
         posts.append(post_db[i].detail)
         posts_id.append(post_db[i].id)
         post_time.append(post_db[i].date_time)
@@ -32,7 +33,7 @@ def main(user):
         if postform.validate_on_submit():
             if postform.chname_button.data: 
                 name_change = True
-                return render_template('index.html', postform = postform, posts=posts, user=user, post_time=post_time, users=users, posts_id=posts_id, name_change=name_change, delete_account=delete_account)
+                return render_template('index.html', range_posts=range_posts, postform = postform, posts=posts, user=user, post_time=post_time, users=users, posts_id=posts_id, name_change=name_change, delete_account=delete_account)
 
             if postform.submit4.data:
                 newname = postform.chname.data
@@ -44,7 +45,7 @@ def main(user):
 
             if postform.submit3.data:
                 delete_account = True
-                return render_template('index.html', postform = postform, posts=posts, user=user, post_time=post_time, users=users, posts_id=posts_id, name_change=name_change, delete_account=delete_account)
+                return render_template('index.html', range_posts=range_posts, postform = postform, posts=posts, user=user, post_time=post_time, users=users, posts_id=posts_id, name_change=name_change, delete_account=delete_account)
 
             if postform.submit5.data:
                 return redirect(url_for('login'))
@@ -78,18 +79,9 @@ def main(user):
             
             return redirect(url_for('main', user=user))
         if postform.errors: 
-            return render_template('index.html', postform = postform, posts=posts, user=user, post_time=post_time, users=users, posts_id=posts_id, name_change=name_change, delete_account=delete_account)
+            return render_template('index.html', range_posts=range_posts, postform = postform, posts=posts, user=user, post_time=post_time, users=users, posts_id=posts_id, name_change=name_change, delete_account=delete_account)
 
-        
-    """ post_db = Post.query.order_by(Post.id).all()
-    posts = []
-    posts_id = []
-    users = []
-    for i in range(len(post_db)):
-        posts.append(post_db[i].detail)
-        posts_id.append(post_db[i].id)
-        users.append(post_db[i].user.name) """
-    return render_template('index.html', postform = postform, posts=posts, user=user, post_time=post_time, users=users, posts_id=posts_id, name_change=name_change, delete_account=delete_account)
+    return render_template('index.html', range_posts=range_posts, postform = postform, posts=posts, user=user, post_time=post_time, users=users, posts_id=posts_id, name_change=name_change, delete_account=delete_account)
 
 @app.route('/login', methods = ['GET','POST'])
 def login(): #Add users
