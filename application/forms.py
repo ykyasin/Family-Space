7fd2275d4b7187c1a1ff1841eb7b762f81ecb0cf
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-03 17:46:31 +0000

    restructuring routes and forms files

--- application/forms.py
+++ application/forms.py
@@ -37,7 +37,7 @@ class PostForm(FlaskForm):
     yesdel = SubmitField('Yes')
     nodel = SubmitField('No')
     chname_button = SubmitField('Change account name')
-    chname = StringField('Input name')
+    chname = StringField('Input name', Optional(strip_whitespace=True))
     submit4 = SubmitField('Change')
     submit5 = SubmitField('Logout')
 
