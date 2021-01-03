8ad8e0ecc7b5090e42e107a2bbc4640e02226a71
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-03 19:35:28 +0000

    fixing postform validators

--- application/routes.py
+++ application/routes.py
@@ -77,8 +77,9 @@ def main(user):
             
             return redirect(url_for('main', user=user))
         if postform.errors: 
-            return str(postform.errors)
+            return render_template('index.html', postform = postform, posts=posts, user=user, post_time=post_time, users=users, posts_id=posts_id, name_change=name_change, delete_account=delete_account)
 
+        
     """ post_db = Post.query.order_by(Post.id).all()
     posts = []
     posts_id = []
