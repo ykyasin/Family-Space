1e53d9a3d9dd1ab927e8fc257b1f98596964fe60
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-03 16:15:15 +0000

    Validating post length fixing errors

--- application/forms.py
+++ application/forms.py
@@ -37,9 +37,7 @@ class PostForm(FlaskForm):
     yesdel = SubmitField('Yes')
     nodel = SubmitField('No')
     chname_button = SubmitField('Change account name')
-    chname = StringField('Input name', validators=[
-        Length(min=2, max=20)
-    ])
+    chname = StringField('Input name')
     submit4 = SubmitField('Change')
     submit5 = SubmitField('Logout')
 
