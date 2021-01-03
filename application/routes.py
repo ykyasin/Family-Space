08a4d91eaf9a4aad4798a4ad2e12549e6f7558c6
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-03 16:36:24 +0000

    Validating post length fixing errors

--- application/routes.py
+++ application/routes.py
@@ -66,8 +66,8 @@ def main(user):
                 return redirect(url_for('main', user=user))
             
             return redirect(url_for('main', user=user))
-        if postform.errors: 
-            return str(postform.errors)
+"""         if postform.errors: 
+            return str(postform.errors) """
 
 
         if chnameform.validate_on_submit:
