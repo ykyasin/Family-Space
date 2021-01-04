6db962469b8c99a8ee8bfd8ebff6847fc9a227ca
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-04 19:40:20 +0000

    initialising database

--- create.py
+++ create.py
@@ -1,14 +1,14 @@
 from application import db 
-from application.models import User
+from application.models import User, Post
 db.drop_all()
 db.create_all()
 
 user1 = User(name='Yusuf')
+user2 = User(name='Ali')
+post1 = Post(detail='My first post', user = user1)
 db.session.add(user1)
-user2 = User(name='Yonis')
 db.session.add(user2)
-user3 = User(name='Ismail')
-db.session.add(user3)
+db.session.add(post1)
 
 
 db.session.commit()
