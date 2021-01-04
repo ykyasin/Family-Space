89c22cb2afabf0f0a7b48a2ea112951128cc8165
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-04 11:04:06 +0000

    Updating Jenkins execution build 2

--- startup.sh
+++ startup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash 
 
+echo "------------------------------------My Tests------------------------------------------"
+ls
 cd /opt/fs-app
 sudo chown -R fs-app
 sudo python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-
-ls
-
-echo "------------------------------------My Tests------------------------------------------"
 
 python3 create.py
 python3 app.py  
