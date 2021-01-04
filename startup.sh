a571895ef66cda25d9cc1589b6a3a04053899a44
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-04 10:56:08 +0000

    updated startup

--- startup.sh
+++ startup.sh
@@ -5,5 +5,10 @@ sudo chown -R fs-app
 sudo python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+
+pytest -cov=application
+
+echo "-------------------------------------My Tests-------------------------------------------"
+
 python3 create.py
 python3 app.py  
