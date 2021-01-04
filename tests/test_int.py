b59ec16e9bac4c7354580ab0d8bfb308ff93b58b
Yusuf-Yasin <ykyasin1996@gmail.com>
2021-01-04 12:08:30 +0000

    updated sh file

--- tests/test_int.py
+++ tests/test_int.py
@@ -21,10 +21,11 @@ class TestBase(LiveServerTestCase):
         """Setup the test driver and create test users"""
         print("--------------------------NEXT-TEST----------------------------------------------")
         chrome_options = Options()
-        #chrome_options.binary_location = "/usr/bin/chromium-browser" /home/ykyasin1996/chromedriver
-        chrome_options.binary_location = "/home/ykyasin1996/chromedriver"
+        chrome_options.binary_location = "/usr/bin/chromium-browser" /home/ykyasin1996/chromedriver
         chrome_options.add_argument("--headless")
-        self.driver = webdriver.Chrome(executable_path="/home/Admin/chromedriver", chrome_options=chrome_options)
+        #self.driver = webdriver.Chrome(executable_path="/home/Admin/chromedriver", chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(executable_path="/home/ykyasin1996/chromedriver", chrome_options=chrome_options)
+        
         self.driver.get("http://localhost:5000")
         db.session.commit()
         db.drop_all()
